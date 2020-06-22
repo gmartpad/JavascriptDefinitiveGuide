4379e9fb0d87cbe049683a20c9eb45f852aa0a2d
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-06-22 18:29:49 -0300

    2.5 - Unicode

--- 2.5 - Unicode/2.5.1 - Unicode Escape Sequences/EscapeSequences.js
+++ 2.5 - Unicode/2.5.1 - Unicode Escape Sequences/EscapeSequences.js
@@ -7,3 +7,5 @@ caf\u00e9 // => 1; access the variable using an escape
 sequence
 caf\u{E9} // => 1; another form of the same escape
 sequence
+
+console.log("\u{1F600}"); // Prints a smiley face emoji
