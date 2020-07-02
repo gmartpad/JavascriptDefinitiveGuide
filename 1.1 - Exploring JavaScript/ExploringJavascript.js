e001d071b2a7886127ea495e150a90da1c30bcb5
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-07-02 16:44:08 -0300

    1.1 - Exploring Javascript

---
+++ 1.1 - Exploring JavaScript/ExploringJavascript.js
@@ -0,0 +1,43 @@
+/*
+ * When learning a new programming language, it’s important to try the
+ * examples in the book, then modify them and try them again to test your
+ * understanding of the language. To do that, you need a JavaScript
+ * interpreter.
+ * 
+ * The easiest way to try out a few lines of JavaScript is to open up the
+ * web developer tools in your web browser (with F12, Ctrl-Shift-I, or
+ * Command-Option-I) and select the Console tab. You can then type
+ * code at the prompt and see the results as you type. Browser developer
+ * tools often appear as panes at the bottom or right of the browser
+ * window, but you can usually detach them as separate windows, 
+ * which is often quite convenient.
+ * 
+ * Another way to try out JavaScript code is to download and install Node
+from https://nodejs.org. Once Node is installed on your system, you
+can simply open a Terminal window and type node to begin an
+interactive JavaScript session like this one:
+ */
+
+/*
+ * $ node
+ * Welcome to Node.js v12.13.0.
+ * Type ".help" for more information.
+ * > .help
+ * .break Sometimes you get stuck, this gets you out
+ * .clear Alias for .break
+ * .editor Enter editor mode
+ * .exit Exit the repl
+ * .help Print this help message
+ * .load Load JS from a file into the REPL session
+ * .save Save all evaluated commands in this REPL session to
+ * a file
+ * Press ^C to abort current expression, ^D to exit the repl
+ * > let x = 2, y = 3;
+ * undefined
+ * > x + y
+ * 5
+ * > (x === 2) && (y === 3)
+ * true
+ * > (x > 3) || (y < 3)
+ * false
+ */
