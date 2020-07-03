767bc7e6e0fe53df6fd923c9f898ca152a92e5a3
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-07-02 21:37:20 -0300

    3.0 - Types, Values, and Variables

---
+++ 3.0 - Types, Values, and Variables/TypesValuesAndVariables.js
@@ -0,0 +1,12 @@
+/*
+ * The kinds of values that can be
+ * represented and manipulated in a programming language are known as
+ * types, and one of the most fundamental characteristics of a
+ * programming language is the set of types it supports. When a program
+ * needs to retain a value for future use, it assigns the value to (or “stores”
+ * the value in) a variable. Variables have names, and they allow use of
+ * those names in our programs to refer to values. The way that variables
+ * work is another fundamental characteristic of any programming
+ * language. This chapter explains types, values, and variables in
+ * JavaScript. It begins with an overview and some definitions.
+ */
