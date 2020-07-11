743a8ef1fab775a4c15fe4fc6802634a1ec71c9d
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-07-11 18:41:58 -0300

    4.7 - Operator Overview

---
+++ 4.7 - Operator Overview/4.7.6 - Order of Evaluation/OrderOfEvaluation.js
@@ -0,0 +1,17 @@
+/*
+ * Operator precedence and associativity specify the order in which
+ * operations are performed in a complex expression, but they do not
+ * specify the order in which the subexpressions are evaluated. JavaScript
+ * always evaluates expressions in strictly left-to-right order. In the
+ * expression w = x + y * z, for example, the subexpression w is
+ * evaluated first, followed by x, y, and z. Then the values of y and z are
+ * multiplied, added to the value of x, and assigned to the variable or
+ * property specified by expression w. Adding parentheses to the
+ * expressions can change the relative order of the multiplication,
+ * addition, and assignment, but not the left-to-right order of evaluation.
+ * 
+ * Order of evaluation only makes a difference if any of the expressions
+ * being evaluated has side effects that affect the value of another
+ * expression. If expression x increments a variable that is used by
+ * expression z, then the fact that x is evaluated before z is important.
+ */
