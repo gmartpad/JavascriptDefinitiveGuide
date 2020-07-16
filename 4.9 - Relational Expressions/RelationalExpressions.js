31cedd38a618369fba304995dbd3b8bf5cad2f9b
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-07-16 20:02:51 -0300

    4.9 - Relational Expressions

---
+++ 4.9 - Relational Expressions/RelationalExpressions.js
@@ -0,0 +1,11 @@
+/*
+ * This section describes JavaScript’s relational operators. These
+ * operators test for a relationship (such as “equals,” “less than,” or
+ * “property of”) between two values and return true or false
+ * depending on whether that relationship exists. Relational expressions
+ * always evaluate to a boolean value, and that value is often used to
+ * control the flow of program execution in if, while, and for
+ * statements. The subsections that follow document the
+ * equality and inequality operators, the comparison operators, and
+ * JavaScript’s other two relational operators, in and instanceof.
+ */
