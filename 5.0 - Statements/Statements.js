3c27c8c67c67836b07aee7b8362940933e4e50a7
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-07-20 19:47:34 -0300

    5.0 - Statements

---
+++ 5.0 - Statements/Statements.js
@@ -0,0 +1,42 @@
+/*
+ * By that
+ * analogy, statements are JavaScript sentences or commands. Just as
+ * English sentences are terminated and separated from one another with
+ * periods, JavaScript statements are terminated with semicolons.
+ * Expressions are evaluated to produce a value, but statements are
+ * executed to make something happen
+ *
+ * One way to “make something happen” is to evaluate an expression that
+ * has side effects. Expressions with side effects, such as assignments and
+ * function invocations, can stand alone as statements, and when used this
+ * way are known as expression statements. A similar category of
+ * statements are the declaration statements that declare new variables
+ * and define new functions.
+ * 
+ * JavaScript programs are nothing more than a sequence of statements to
+ * execute. By default, the JavaScript interpreter executes these
+ * statements one after another in the order they are written. Another way
+ * to “make something happen” is to alter this default order of execution,
+ * and JavaScript has a number of statements or control structures that do
+ * just this:
+ * 
+ * Conditionals
+ *      Statements like if and switch that make the JavaScript
+ *      interpreter execute or skip other statements depending on the value
+ *      of an expression
+ * 
+ * Loops
+ *      Statements like while and for that execute other statements
+ *      repetitively
+ * 
+ * Jumps
+ *      Statements like break, return, and throw that cause the
+ *      interpreter to jump to another part of the program
+ * 
+ * The sections that follow describe the various statements in JavaScript
+ * and explain their syntax. Table 5-1, at the end of the chapter,
+ * summarizes the syntax. A JavaScript program is simply a sequence of
+ * statements, separated from one another with semicolons, so once you
+ * are familiar with the statements of JavaScript, you can begin writing
+ * JavaScript programs.
+ */
