07b4893295cff42699742bb3f4ad20e0fceea1fa
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-07-20 01:47:21 -0300

    4.14 - Summary

---
+++ 4.14 - Summary/Summary.js
@@ -0,0 +1,30 @@
+/*
+ * This chapter covers a wide variety of topics, and there is lots of
+ * reference material here that you may want to reread in the future as you
+ * continue to learn JavaScript. Some key points to remember, however,
+ * are these:
+ *
+ *      - Expressions are the phrases of a JavaScript program.
+ *      
+ *      - Any expression can be evaluated to a JavaScript value.
+ *      
+ *      - Expressions can also have side effects (such as variable
+ *      assignment) in addition to producing a value.
+ *      
+ *      - Simple expressions such as literals, variable references, and
+ *      property accesses can be combined with operators to produce
+ *      larger expressions.
+ *      
+ *      - JavaScript defines operators for arithmetic, comparisons,
+ *      Boolean logic, assignment, and bit manipulation, along with
+ *      some miscellaneous operators, including the ternary
+ *      conditional operator.
+ *      
+ *      - The JavaScript + operator is used to both add numbers and
+ *      concatenate strings.
+ *      
+ *      - The logical operators && and || have special “shortcircuiting” 
+ *      behavior and sometimes only evaluate one of their
+ *      arguments. Common JavaScript idioms require you to
+ *      understand the special behavior of these operators.
+ */
