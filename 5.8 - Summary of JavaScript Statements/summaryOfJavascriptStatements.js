49081bf4b2bde0eb00b866149def32846faa5a1c
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-07-24 16:54:10 -0300

    5.8 - Summary of Javascript Statements

---
+++ 5.8 - Summary of JavaScript Statements/summaryOfJavascriptStatements.js
@@ -0,0 +1,60 @@
+/*
+ * Statement            Purpose
+ * 
+ * break                Exit from the innermost loop or switch or from named enclosing
+ *                      statement
+ * 
+ * case                 Label a statement within a switch
+ * 
+ * class                Declare a class
+ * 
+ * const                Declare and initialize one or more constants
+ * 
+ * continue             Begin next iteration of the innermost loop or the named loop
+ * 
+ * debugger             Debugger breakpoint
+ * 
+ * default              Label the default statement within a switch
+ * 
+ * do/while             An alternative to the while loop
+ * 
+ * export               Declare values that can be imported into other modules
+ * 
+ * for                  An easy-to-use loop
+ * 
+ * for/await            Asynchronously iterate the values of an async iterator
+ * 
+ * for/in               Enumerate the property names of an object
+ * 
+ * for/of               Enumerate the values of an iterable object such as an array
+ * 
+ * function             Declare a function
+ * 
+ * if/else              Execute one statement or another depending on a condition
+ * 
+ * import               Declare names for values defined in other modules
+ * 
+ * label                Give statement a name for use with break and continue
+ * 
+ * let                  Declare and initialize one or more block-scoped variables (new
+ *                      syntax)
+ * 
+ * return               Return a value from a function
+ * 
+ * switch               Multiway branch to case or default: labels
+ * 
+ * throw                Throw an exception
+ * 
+ * try/catch/final      Handle exceptions and code cleanup
+ * ly
+ * 
+ * “use strict”         Apply strict mode restrictions to script or function
+ * 
+ * var                  Declare and initialize one or more variables (old syntax)
+ * 
+ * while                A basic loop construct
+ * 
+ * with                 Extend the scope chain (deprecated and forbidden in strict mode)
+ * 
+ * yield                Provide a value to be iterated; only used in generator functions
+ */
