c2541701a4476d668ad5894ad90b35cf07ef3202
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-07-31 19:01:23 -0300

    6.11 - Summary

---
+++ 6.11 - Summary/summary.js
@@ -0,0 +1,24 @@
+/*
+ * This chapter has documented JavaScript objects in great detail,
+ * covering topics that include:
+ * 
+ *      - Basic object terminology, including the meaning of terms like
+ *      enumerable and own property.
+ * 
+ *      - Object literal syntax, including the many new features in ES6
+ *      and later.
+ * 
+ *      - How to read, write, delete, enumerate, and check for the
+ *      presence of the properties of an object.
+ * 
+ *      - How prototype-based inheritance works in JavaScript and how
+ *      to create an object that inherits from another object with
+ *      Object.create().
+ * 
+ *      - How to copy properties from one object into another with
+ *      Object.assign().
+ * 
+ *      - All JavaScript values that are not primitive values are objects. This
+ *      includes both arrays and functions, which are the topics of the next two
+ *      chapters.
+ */
