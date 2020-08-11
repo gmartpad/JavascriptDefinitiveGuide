a693625eaa7fbdf34e33a2c4cd4846b071e5d0da
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-08-10 21:36:50 -0300

    7.11 - Summary

---
+++ 7.11 - Summary/summary.js
@@ -0,0 +1,18 @@
+/*
+ * This chapter has covered JavaScript arrays in depth, including esoteric
+ * details about sparse arrays and array-like objects. The main points to
+ * take from this chapter are:
+ * 
+ *      - Array literals are written as comma-separated lists of values
+ *      within square brackets.
+ *      
+ *      - Individual array elements are accessed by specifying the
+ *      desired array index within square brackets.
+ * 
+ *      - The for/of loop and ... spread operator introduced in ES6
+ *      are particularly useful ways to iterate arrays.
+ * 
+ *      - The Array class defines a rich set of methods for manipulating
+ *      arrays, and you should be sure to familiarize yourself with the
+ *      Array API.
+ */
